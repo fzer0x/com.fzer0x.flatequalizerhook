f3d2e6168649ebf668e7b33bb12b804457e8fdb4
fzer0x <57650139+fzer0x@users.noreply.github.com>
2026-01-13 19:33:03 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.fzer0x.flatequalizerhook"
         minSdk = 29
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.2"
+        versionCode = 15
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
